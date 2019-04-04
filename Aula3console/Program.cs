afa686826b6db3bbbd396f78871142da7217c952
Danielly Araujo <daraujo@iauditeden.onmicrosoft.com>
2019-04-04 11:05:52 -0300

    Add project files.

---
+++ Aula3console/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Aula3console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Uma escola de contratou para desenvolver um app que realize a media das notas dos alunos de uma sala ,
+            //lembrando que a sala tem 5 alunos e anota vai de 0 a 10.0
+
+            Console.WriteLine("Média de Alunos");
+
+            float soma = 0;
+            float media = 0;
+
+            for (int i =0; i <5 ; )
+            {
+
+            }
+
+        }
+    }
+}
